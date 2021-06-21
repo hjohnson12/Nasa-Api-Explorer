564be2233b1c6aa702fa14a61960ac052d43f236
hjohnson12 <hunter.johnson512@gmail.com>
2021-06-21 00:02:35 -0400

    Added enum for persev. rover camera names

--- NasaDataExplorer/Models/MarsRoverPhotoData.cs
+++ NasaDataExplorer/Models/MarsRoverPhotoData.cs
@@ -51,6 +51,70 @@ namespace NasaDataExplorer.Models
             MINITES   
         }
 
+        public enum PerseveranceCameraNames
+        {
+            /// <summary>
+            /// Rover Up-Look Camera
+            /// </summary>
+            EDL_RUCAM,
+            /// <summary>
+            /// Rover Down-Look Camera
+            /// </summary>
+            EDL_RDCAM,
+            /// <summary>
+            /// Descent Stage Down-Look Camera
+            /// </summary>
+            EDL_DDCAM,
+            /// <summary>
+            /// Parachute Up-Look Camera A
+            /// </summary>
+            EDL_PUCAM1,
+            /// <summary>
+            /// Parachute Up-Look Camera B
+            /// </summary>
+            EDL_PUCAM2,
+            /// <summary>
+            /// Navigation Camera - Left
+            /// </summary>
+            NAVCAM_LEFT,
+            /// <summary>
+            /// Navigation Camera - Right
+            /// </summary>
+            NAVCAM_RIGHT,
+            /// <summary>
+            /// Mast Camera Zoom - Right
+            /// </summary>
+            MCZ_RIGHT,
+            /// <summary>
+            /// Mast Camera Zoom - Left
+            /// </summary>
+            MCZ_LEFT,
+            /// <summary>
+            ///  Front Hazard Avoidance Camera - Left
+            /// </summary>
+            FRONT_HAZCAM_LEFT_A,
+            /// <summary>
+            /// Front Hazard Avoidance Camera - Right
+            /// </summary>
+            FRONT_HAZCAM_RIGHT_A,
+            /// <summary>
+            /// Rear Hazard Avoidance Camera - Left
+            /// </summary>
+            REAR_HAZCAM_LEFT,
+            /// <summary>
+            /// Rear Hazard Avoidance Camera - Right
+            /// </summary>
+            REAR_HAZCAM_RIGHT,
+            /// <summary>
+            /// MEDA Skycam
+            /// </summary>
+            SKYCAM,
+            /// <summary>
+            /// SHERLOC WATSON Camera
+            /// </summary>
+            SHERLOC_WATSON
+        }
+
         private IEnumerable<MarsRoverPhoto> _photos;
 
         [JsonProperty("photos")]
