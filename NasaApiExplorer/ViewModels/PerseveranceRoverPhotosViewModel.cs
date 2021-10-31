6961f1a198b757ddbfc9881f1e1e979e99556a8b
hjohnson12 <hunter.johnson512@gmail.com>
2021-10-31 13:07:37 -0400

    Setup persev. rover photos to use base view model

--- NasaApiExplorer/ViewModels/PerseveranceRoverPhotosViewModel.cs
+++ NasaApiExplorer/ViewModels/PerseveranceRoverPhotosViewModel.cs
@@ -12,36 +12,20 @@ using NasaApiExplorer.Services.NasaApis;
 
 namespace NasaApiExplorer.ViewModels
 {
-    public class PerseveranceRoverPhotosViewModel : Base.Observable
+    public class PerseveranceRoverPhotosViewModel : RoverPhotosBaseViewModel
     {
-        private readonly INasaApiService _nasaApiService;
-        private readonly IFileDownloadService _fileDownloadService;
-        private readonly IDialogService _dialogService;
         private const string DEFAULT_COMBO_OPTION = "- Choose Camera (optional) -";
         private MarsRover _perseveranceRover;
-        private ObservableCollection<MarsRoverPhoto> _perseverancePhotos;
-        private ObservableCollection<string> _roverCameras;
-        private ObservableCollection<string> _roverCameras2;
-        private bool _isLoading;
-        private DateTimeOffset? _selectedDate;
-        private string _selectedCamera;
         private CancellationTokenSource _cancellationTokenSource;
 
         public ICommand LoadPhotosCommand { get; set; }
-        public ICommand DownloadPhotosCommand { get; set; }
-        public ICommand SelectPhotoCommand { get; set; }
-        public ICommand UpdateDateCommand { get; set; }
-        public ICommand UpdateSelectedCameraCommand { get; set; }
 
         public PerseveranceRoverPhotosViewModel(
             INasaApiService nasaApiService,
             IFileDownloadService fileDownloadService,
             IDialogService dialogService)
+            : base(nasaApiService, fileDownloadService, dialogService)
         {
-            _nasaApiService = nasaApiService;
-            _fileDownloadService = fileDownloadService;
-            _dialogService = dialogService;
-
             var cameraList = MarsRoverPhotoData.PerseveranceCameras;
             _roverCameras2 = new ObservableCollection<string>(
                 cameraList.Select(x => x.Item2.ToString())
@@ -49,78 +33,13 @@ namespace NasaApiExplorer.ViewModels
             //_roverCameras2.Insert(0, DEFAULT_COMBO_OPTION);
 
             SelectedDate = DateTimeOffset.Now.AddDays(1);
-            PerseverancePhotos = new ObservableCollection<MarsRoverPhoto>();
+            RoverPhotos = new ObservableCollection<MarsRoverPhoto>();
 
             LoadPhotosCommand =
                 new AsyncRelayCommand(LoadPerseveranceRoverPhotos);
-            UpdateDateCommand =
-                new Base.RelayCommand<DateTimeOffset?>(UpdateSelectedDate);
-            SelectPhotoCommand =
-                new AsyncRelayCommand<MarsRoverPhoto>(DisplayPhoto);
-            UpdateSelectedCameraCommand =
-                new Base.RelayCommand<string>(UpdateSelectedCamera);
-            DownloadPhotosCommand =
-                new AsyncRelayCommand(DownloadPhotos);
         }
 
         public MarsRover PerseveranceRover { get; set; }
-
-        public ObservableCollection<MarsRoverPhoto> PerseverancePhotos
-        {
-            get => _perseverancePhotos;
-            set
-            {
-                SetProperty(ref _perseverancePhotos, value);
-                OnPropertyChanged("IsPhotosAvailable");
-            }
-        }
-
-        /// <summary>
-        /// Determines if there are photos loaded.
-        /// </summary>
-        public bool IsPhotosAvailable => PerseverancePhotos.Count == 0;
-
-        public bool IsLoading
-        {
-            get => _isLoading;
-            set => SetProperty(ref _isLoading, value);
-        }
-
-        public ObservableCollection<string> RoverCameras
-        {
-            get => _roverCameras;
-            set => SetProperty(ref _roverCameras, value);
-        }
-
-        public ObservableCollection<string> RoverCameras2
-        {
-            get => _roverCameras2;
-            set => SetProperty(ref _roverCameras2, value);
-        }
-
-        public DateTimeOffset? SelectedDate
-        {
-            get => _selectedDate;
-            set
-            {
-                _selectedDate = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string SelectedCamera
-        {
-            get => _selectedCamera;
-            set
-            {
-                _selectedCamera = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public void UpdateSelectedDate(DateTimeOffset? date) => SelectedDate = date;
-
-        public void UpdateSelectedCamera(string camera) => SelectedCamera = camera;
 
         /// <summary>
         /// Retrieves photos from perseverance rover using photos service.
@@ -146,14 +65,14 @@ namespace NasaApiExplorer.ViewModels
                         .Single(x => x.Item2.Equals(SelectedCamera))
                         .Item1;
 
-                    PerseverancePhotos =
+                    RoverPhotos =
                         new ObservableCollection<MarsRoverPhoto>(
                             await _nasaApiService.MarsRoverPhotos.GetPerseveranceRoverPhotosAsync(
                                 photosDate, camera, cancellationToken));
                 }
                 else
                 {
-                    PerseverancePhotos =
+                    RoverPhotos =
                         new ObservableCollection<MarsRoverPhoto>(
                             await _nasaApiService.MarsRoverPhotos.GetPerseveranceRoverPhotosAsync(
                                 photosDate, cancellationToken));
@@ -176,49 +95,6 @@ namespace NasaApiExplorer.ViewModels
                 IsLoading = false;
                 _cancellationTokenSource = null;
             }
-        }
-
-        public bool IsCameraSelected(string selection) => !string.IsNullOrEmpty(selection);
-
-        /// <summary>
-        /// Formats the given date accordingly to match api request pattern.
-        /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
-        public string FormatDate(DateTimeOffset? date)
-        {
-            return date.Value.Year.ToString()
-                + "-" + date.Value.Month.ToString()
-                + "-" + date.Value.Day.ToString();
-        }
-
-        /// <summary>
-        /// Downloads rover photos for the selected date in a location chosen 
-        /// through a folder picker.
-        /// </summary>
-        /// <returns></returns>
-        public async Task DownloadPhotos()
-        {
-            string[] urls = PerseverancePhotos.Select(x => x.ImageSourceUrl).ToArray();
-
-            try
-            {
-                await _fileDownloadService.DownloadFiles(urls);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        /// <summary>
-        /// Displays the selected photo in a dialog.
-        /// </summary>
-        /// <param name="roverPhoto"></param>
-        /// <returns></returns>
-        public async Task DisplayPhoto(MarsRoverPhoto roverPhoto)
-        {
-            await _dialogService.ShowPhotoDialog(roverPhoto, PerseverancePhotos.ToList());
         }
     }
 }
---
+++ NasaApiExplorer/ViewModels/RoverPhotosBaseViewModel.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using NasaApiExplorer.Models;
+using NasaApiExplorer.Services;
+using NasaApiExplorer.Services.NasaApis;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NasaApiExplorer.ViewModels
+{
+    public abstract class RoverPhotosBaseViewModel : Base.Observable
+    {
+        protected readonly INasaApiService _nasaApiService;
+        protected readonly IFileDownloadService _fileDownloadService;
+        protected readonly IDialogService _dialogService;
+        protected ObservableCollection<MarsRoverPhoto> _roverPhotos;
+        protected ObservableCollection<string> _roverCameras;
+        protected ObservableCollection<string> _roverCameras2;
+        protected bool _isLoading;
+        protected DateTimeOffset? _selectedDate;
+        protected string _selectedCamera;
+
+        public ICommand DownloadPhotosCommand { get; set; }
+        public ICommand SelectPhotoCommand { get; set; }
+        public ICommand UpdateDateCommand { get; set; }
+        public ICommand UpdateSelectedCameraCommand { get; set; }
+
+        protected RoverPhotosBaseViewModel(
+            INasaApiService nasaApiService,
+            IFileDownloadService fileDownloadService,
+            IDialogService dialogService)
+        {
+            _nasaApiService = nasaApiService;
+            _fileDownloadService = fileDownloadService;
+            _dialogService = dialogService;
+
+            UpdateDateCommand =
+               new Base.RelayCommand<DateTimeOffset?>(UpdateSelectedDate);
+            SelectPhotoCommand =
+                new AsyncRelayCommand<MarsRoverPhoto>(DisplayPhoto);
+            UpdateSelectedCameraCommand =
+                new Base.RelayCommand<string>(UpdateSelectedCamera);
+            DownloadPhotosCommand =
+                new AsyncRelayCommand(DownloadPhotos);
+        }
+
+        public ObservableCollection<MarsRoverPhoto> RoverPhotos
+        {
+            get => _roverPhotos;
+            set
+            {
+                SetProperty(ref _roverPhotos, value);
+                OnPropertyChanged("IsPhotosAvailable");
+            }
+        }
+
+        public bool IsPhotosAvailable => RoverPhotos.Count == 0;
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => SetProperty(ref _isLoading, value);
+        }
+
+        public ObservableCollection<string> RoverCameras
+        {
+            get => _roverCameras;
+            set => SetProperty(ref _roverCameras, value);
+        }
+
+        public ObservableCollection<string> RoverCameras2
+        {
+            get => _roverCameras2;
+            set => SetProperty(ref _roverCameras2, value);
+        }
+
+        public DateTimeOffset? SelectedDate
+        {
+            get => _selectedDate;
+            set
+            {
+                _selectedDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedCamera
+        {
+            get => _selectedCamera;
+            set
+            {
+                _selectedCamera = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void UpdateSelectedDate(DateTimeOffset? date) => SelectedDate = date;
+        public void UpdateSelectedCamera(string camera) => SelectedCamera = camera;
+        public bool IsCameraSelected(string selection) => !string.IsNullOrEmpty(selection);
+
+        /// <summary>
+        /// Downloads rover photos for the selected date in a location chosen 
+        /// through a folder picker.
+        /// </summary>
+        /// <returns></returns>
+        public async Task DownloadPhotos()
+        {
+            string[] urls = RoverPhotos.Select(x => x.ImageSourceUrl).ToArray();
+
+            try
+            {
+                await _fileDownloadService.DownloadFiles(urls);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Formats the given date accordingly to match api request pattern.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public string FormatDate(DateTimeOffset? date)
+        {
+            return date.Value.Year.ToString()
+                + "-" + date.Value.Month.ToString()
+                + "-" + date.Value.Day.ToString();
+        }
+
+        /// <summary>
+        /// Displays the selected photo in a dialog.
+        /// </summary>
+        /// <param name="roverPhoto"></param>
+        /// <returns></returns>
+        public async Task DisplayPhoto(MarsRoverPhoto roverPhoto)
+        {
+            await _dialogService.ShowPhotoDialog(roverPhoto, RoverPhotos.ToList());
+        }
+    }
+}
